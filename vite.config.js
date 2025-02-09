02e6bd9dbd081e394b5c01ce8478973c96f2a720
Harsh Sharma <99297487+Harshbh7@users.noreply.github.com>
2025-02-09 21:20:15 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {}
+    'process.env': {},
+    "process.env.NODE_ENV": JSON.stringify("development"),
   }, 
-  base: '/Chat-App/',
+  base: 'https://harshbh7.github.io/Chat-App/',
 });
